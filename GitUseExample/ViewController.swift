9e4a57a1dc7c58beb8381868879bce0f385ef087
佐藤真樹 <satoms@satoumakinoMacBook-Pro.local>
2017-07-04 12:35:54 +0900

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by 佐藤真樹 on 2017/07/04.
+//  Copyright © 2017年 Misubo Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
