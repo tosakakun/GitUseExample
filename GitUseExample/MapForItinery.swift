30efd912dee67d8456d27b1dfd22b3b36dbc388b
佐藤真樹 <satoms@satoumakinoMacBook-Pro.local>
2017-07-04 13:11:10 +0900

    Add map feauture

---
+++ GitUseExample/MapForItinery.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinery.swift
+//  GitUseExample
+//
+//  Created by 佐藤真樹 on 2017/07/04.
+//  Copyright © 2017年 Misubo Company. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinery: NSObject {
+
+}
--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -27,3 +27,9 @@ class ViewController: UIViewController {
 
 }
 
+
+
+
+
+
+
